3e59a7fe1de910e274a2067de5236cdecd5cac34
alfarezyyd <adityaalfarezyd@gmail.com>
2024-03-23 18:52:56 +0700

    Add NextUI and some package

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+// eslint-disable-next-line @typescript-eslint/no-var-requires,no-undef
+const {nextui} = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+    content: [
+        // ...
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+        extend: {},
+    },
+    darkMode: "class",
+    plugins: [nextui()]
+}
